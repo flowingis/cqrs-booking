040a08576ec0f634a809b0956453a1ada37fab6e
saverio <s.tosi@extrategy.net>
2018-03-30 17:27:47 +0200

    create first route

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function index()
+    {        
+        return new Response(
+            '<html><body>Hello world!</body></html>'
+        );
+    }
+}
